f13bdc3832aa934757aed09c0cf4e8d56623e710
Thilak CM <62937393+Thilak-cm@users.noreply.github.com>
2025-04-25 14:53:02 -0400

    renamed .sh and dockerfiles following good naming convention

---
+++ run_training_xgbr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to build and run the XGBoost model training container
+# Mounts data and models directories for training and model persistence
+
+# Build the image
+# Uncomment after first run
+docker build -t xgbr-model-training Dockerfile.xgbr
+
+# Run the container with volume mounts
+docker run --rm \
+  -v "$(pwd)/data_from_2024:/app/data_from_2024" \
+  -v "$(pwd)/models:/app/models" \
+  xgbr-model-training
